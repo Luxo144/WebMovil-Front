fddccb8bfb4091e714d13cbaf45935fa88a9942c
FelipeIgnacioS <89607222+FelipeIgnacioS@users.noreply.github.com>
2023-10-20 03:42:20 -0300

    commit funciones

--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -1,6 +1,35 @@
-import { RegisterData, LoginData, LoginResponse, PasswordResetRequest, PasswordReset,User } from '../types/user';
+import { RegisterData, LoginData, LoginResponse, PasswordResetRequest, PasswordReset,User, CreateProfileData, UpdateProfileData, UpdateUserData } from '../types/user';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import jwt_decode from 'jsonwebtoken';
 
 const BASE_URL = 'http://10.0.2.2:3000/auth';
+const TOKEN_STORAGE_KEY = '@auth_token';
+
+export const storeToken = async (token: string): Promise<void> => {
+  try {
+    await AsyncStorage.setItem(TOKEN_STORAGE_KEY, token);
+  } catch (e) {
+    console.error("Error al guardar el token", e);
+  }
+};
+
+export const getToken = async (): Promise<string | null> => {
+  try {
+    return await AsyncStorage.getItem(TOKEN_STORAGE_KEY);
+  } catch (e) {
+    console.error("Error al obtener el token", e);
+    return null;
+  }
+};
+
+export const removeToken = async (): Promise<void> => {
+  try {
+    await AsyncStorage.removeItem(TOKEN_STORAGE_KEY);
+  } catch (e) {
+    console.error("Error al remover el token", e);
+  }
+};
+
 
 export const register = async (userData: RegisterData): Promise<User> => {
   const response = await fetch(`${BASE_URL}/register`, {
@@ -24,12 +53,13 @@ export const login = async (loginData: LoginData): Promise<LoginResponse> => {
     body: JSON.stringify(loginData),
   });
 
-  if (!response.ok) { // Si el código de estado HTTP no está en el rango 200-299
-    const errorData = await response.json(); // Puedes agregar esto si el backend envía datos adicionales en el error
-    throw new Error(errorData.message || 'Error durante el inicio de sesión'); // Lanza un error
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.message || 'Error durante el inicio de sesión');
   }
 
   const data: LoginResponse = await response.json();
+  await storeToken(data.accessToken); // Guardamos el token en AsyncStorage después de iniciar sesión exitosamente
   return data;
 };
 
@@ -59,4 +89,101 @@ export const resetPassword = async (resetData: PasswordReset): Promise<void> => 
     },
     body: JSON.stringify(resetData),
   });
+};
+
+export const getProfile = async (id: number): Promise<User> => {
+  const response = await fetch(`${BASE_URL}/${id}`, {
+      method: 'GET',
+  });
+  
+  if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message || 'Error fetching profile');
+  }
+
+  const data: User = await response.json();
+  return data;
+};
+
+export const getUserDetails = async (userId: number, token: string): Promise<Partial<User>> => {
+  const response = await fetch(`${BASE_URL}/user/${userId}`, {
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+      },
+  });
+
+  if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message || 'Error fetching user details');
+  }
+
+  const fullData: User = await response.json();
+  const filteredData = {
+      name: fullData.first_name,
+      email: fullData.email
+  };
+  return filteredData;
+};
+
+
+export const createProfile = async (profileData: CreateProfileData): Promise<User> => {
+  const response = await fetch(BASE_URL, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(profileData),
+  });
+
+  if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message || 'Error creating profile');
+  }
+
+  const data: User = await response.json();
+  return data;
+};
+
+export const updateProfile = async (id: number, profileData: UpdateProfileData): Promise<void> => {
+  const token = await getToken();
+  if (!token) {
+      throw new Error("No auth token found");
+  }
+
+  const response = await fetch(`${BASE_URL}/profile/${id}`, {
+      method: 'PUT',
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+      },
+      body: JSON.stringify(profileData),
+  });
+
+  if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message || 'Error updating profile');
+  }
+};
+
+export const updateUser = async (id: number, userData: UpdateUserData): Promise<void> => {
+  const token = await getToken();
+  if (!token) {
+      throw new Error("No auth token found");
+  }
+
+  const response = await fetch(`${BASE_URL}/user/${id}`, {
+      method: 'PUT',
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+      },
+      body: JSON.stringify(userData),
+  });
+
+  if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message || 'Error updating user');
+  }
 };
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,4 +1,3 @@
-
 export interface User {
     id?: number;
     first_name: string;
@@ -6,6 +5,48 @@ export interface User {
     password?: string;
   }
 
+export interface Token{
+  id: number;
+  token: string;
+  type: string;
+  expires_at: Date;
+  user_id: number;
+}
+
+export interface profile{
+  id: number;
+  user_id: number;
+  image: string;
+  name_: string;
+  nickname: string;
+  job_title: string;
+  organization: string;
+  ubication: string;
+  phone: string;
+}
+
+export interface PasswordReset{
+  id: number;
+  user_id: number;
+  token: string;
+  expires_at: Date;
+}
+
+export interface CreateProfileData {
+  user: User;
+  image?: string;
+  name_: string;
+  nickname?: string;
+  job_title?: string;
+  organization?: string;
+  ubication?: string;
+  phone?: string;
+}
+
+export interface LoginResponseDto {
+  accessToken: string;
+  expiresIn: Date;
+}
 
 export interface RegisterData {
     first_name: string;
@@ -28,7 +69,22 @@ export interface RegisterData {
   }
   
   export interface PasswordReset {
+    email: string;
     token: string;
     newPassword: string;
+  }
+
+  export interface UpdateProfileData {
+    name_: string;
+    nickname: string;
+    job_title: string;
+    organization: string;
+    ubication?: string;
+    phone?: string;
+  }
+
+  export interface UpdateUserData {
+    name: string;
+    email: string;
   }
   
